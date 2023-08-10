8fbb66e5138133864aa5b140f33d4861976fd4b6
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-08-10 15:01:15 +0530

    Day 7 commit

---
+++ 30_printing_star_pattern_23.cpp
@@ -0,0 +1,46 @@
+// 29_printing_star_pattern_22
+
+
+/*To print :-
+
+      1
+    2 3
+  4 5 6
+
+
+*/ 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1,n;
+    cout << "Enter Number of line/row : ";
+    cin >>n;
+    int k =  1;
+    
+    while (i<=n)
+    {   
+       int j = n-i,p=1;
+       
+        while (j>0)
+        {
+            cout << "  ";
+            j--;
+        }
+         while (p<=i)
+        {
+           
+            cout << "*" << " ";
+            p++;
+            k++;
+        }
+        
+        
+        cout <<endl;
+        i++;
+
+        
+    }
+    
+}
