22aacb7dbed5edf8b94861541c1ff25aa0761a09
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-08-14 23:36:42 +0530

    Day 9 commit

---
+++ 40_bitwise_operator.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b=6;
+    cout <<"4 and 6 = " << (a & b) <<endl;
+    cout <<"4 or 6 = " << (6 | 1) <<endl;
+    cout <<"Not of 4  = " << ~ a <<endl;
+    cout <<"4 XOR 6 = " << (a ^ b) <<endl <<endl;
+
+    cout << " 15>>1 :" << (15>>1) <<endl;
+    cout << " 7>>1 :" << (7>>1) <<endl;
+    cout << " 9<<1 :" << (3<<1) <<endl;
+    
+
+}
